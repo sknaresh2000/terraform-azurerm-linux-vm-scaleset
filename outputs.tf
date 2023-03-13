9383b5bc2b0d60bf33657d8a15e16f1666083d6d
NareshKumaran Sathiaseelan <naresh@nareshkransmini.mynetworksettings.com>
2023-03-13 14:28:27 -0400

    Added conditional outputs for identity

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,6 @@ output "id" {
 }
 
 output "principal_id" {
-  value       = azurerm_linux_virtual_machine_scale_set.vmss.identity[0].principal_id
+  value       = var.identity_info != null ? azurerm_linux_virtual_machine_scale_set.vmss.identity[0].principal_id : null
   description = "The Managed Identity used by this Virtual Machine scale set"
 }
